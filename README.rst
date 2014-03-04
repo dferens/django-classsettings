8c7b502c6c8ee9e17ae446697f8ad92cd0c8229c
Ferens Dmitriy <ferensdima@gmail.com>
2014-03-04 20:32:47 +0200

    Added project code, improved README

---
+++ README.rst
@@ -0,0 +1,16 @@
+django-classsettings
+====================
+
+Adds ability to define Django settings with classes like this::
+
+  from classsettings import settings
+  
+  @settings
+  class Static(object):
+      def STATIC_URL(self): return '/static/'
+
+  # Transforms into
+  STATIC_URL = '/static/'
+
+As many text editors and IDEs indexes code symbols, with such approach you can
+easily navigate to any group and any line of your settings file.
---
+++ classsettings/settings.py
@@ -0,0 +1,93 @@
+import inspect
+import importlib
+import functools
+import os
+from operator import itemgetter
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def defaultargs(func):
+    """
+    Calls parameterized decorator automatically if no args were
+    supplied.
+    
+    With : @mydecorator
+    Without: @mydecorator()
+    """
+    @functools.wraps(func)
+    def decorated(*args, **kwargs):
+        if len(args) == 1 and callable(args[0]):
+            return func()(args[0])
+        else:
+            return func(*args, **kwargs)
+    return decorated
+
+
+@defaultargs
+def settings(to_dict=False):
+    """
+    Adds ability to define Django settings with classes.
+
+    With `to_dict` mode on, calls each public method of decorated class and
+    returns dictionary of it's results.
+    With `to_dict` mode off, calls each public method of decorated class and
+    injects it's values into it's module scope.
+    """
+    def _convert_to_property(Class):
+        for name, method in _get_attributes(Class):
+            setattr(Class, name, property(method))
+
+    def _get_attributes(ClassInstance):
+        members = sorted(inspect.getmembers(ClassInstance, inspect.ismethod),
+                         key=itemgetter(0))
+        return filter(lambda (name, member): not name.startswith('_'), members)
+
+    def attrs_injector(Class):
+        Class._instance = instance = Class()
+        module = importlib.import_module(Class.__module__)
+        for name, value in _get_attributes(instance):
+            setattr(module, name, value())
+        return Class
+
+    def dict_maker(Class):
+        Class._instance = instance = Class()
+        result = dict(_get_attributes(instance))
+        for key in result: result[key] = result[key]()
+        return result
+
+    return dict_maker if to_dict else attrs_injector
+
+
+@defaultargs
+def from_env(key=None):
+    """
+    Gets environment variable by given key.
+    If key is not given, uses decorated function's name.
+    If key is not present within environ, calls given function and raises
+    :class:`ImproperlyConfigured` if it returns `None`.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def decorated(*args, **kwargs):
+            try:
+                return get_env_setting(key or func.__name__)
+            except ImproperlyConfigured, e:
+                default = func(*args, **kwargs)
+                if default is not None: return default
+                raise e
+
+        return decorated
+    return decorator
+
+
+def get_env_setting(setting):
+    """
+    Gets the environment setting and raises exception if it isn't present.
+    """
+    try:
+        return os.environ[setting]
+    except KeyError:
+        error_msg = "Set the %s env variable" % setting
+        raise ImproperlyConfigured(error_msg)
+
